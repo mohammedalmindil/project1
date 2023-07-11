14be0cd7e822077ea0041ac2dd5c809d3baabf06
mohammedalmindil <mohammed.nasser.almindil@gmail.com>
2023-07-11 14:27:49 +0300

    4

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,7 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-
+from encyclopedia import util
 from . import util
-
+from markdown2 import Markdown
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -9,7 +10,22 @@ def index(request):
     })
 
 def CreatePage(request):
-    return render(request,"encyclopedia/createPage.html")
+    if request.method=="POST":
+        form= request.POST
+        title= form["title"]
+        content= form["content"]
+        util.save_entry(title,content)
+        return render (request, "encyclopedia/randompage.html")
+    else:     
+        return render(request,"encyclopedia/createPage.html")
 
 def RandomPage(request):
     return render(request,"encyclopedia/randompage.html")
+def EntryPage(request):
+    if request.method=="GET":
+        form= request.POST
+        title=form["title"]
+        listE=util.list_entries
+        if title in listE:
+            markdown=Markdown()
+            markdown.convert(title)
