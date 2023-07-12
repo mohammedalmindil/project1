4acd38b5340896389ce7e7f0ee0ac41f37360d3d
mohammedalmindil <mohammed.nasser.almindil@gmail.com>
2023-07-13 02:56:49 +0300

    3

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("CreatePage", views.CreatePage,name="createpage"),
-    path("RandomPage", views.RandomPage,name="randompage")
+    path("RandomPage", views.RandomPage,name="randompage"),
+    path("ResultPage",views.ResultPage, name="result"),
+    path("wiki/<str:name>",views.EntryPage ,name="entrypage")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -2,30 +2,66 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from encyclopedia import util
 from . import util
-from markdown2 import Markdown
+import markdown
+
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+
 def CreatePage(request):
     if request.method=="POST":
         form= request.POST
         title= form["title"]
         content= form["content"]
-        util.save_entry(title,content)
-        return render (request, "encyclopedia/randompage.html")
+        if Duplication(title)== True:
+            return render(request,"encyclopedia/result.html",{
+                "Result": "Error" ,
+                "descrption":"entry already exists"
+            })
+        else:
+            util.save_entry(title,content)
+            return render(request,"encyclopedia/result.html",{
+                "Result": "Success" ,
+                "descrption":"entry successfully added"
+            })
     else:     
         return render(request,"encyclopedia/createPage.html")
+    
 
 def RandomPage(request):
     return render(request,"encyclopedia/randompage.html")
-def EntryPage(request):
-    if request.method=="GET":
-        form= request.POST
-        title=form["title"]
-        listE=util.list_entries
-        if title in listE:
-            markdown=Markdown()
-            markdown.convert(title)
+
+
+def Duplication(entry):
+    if util.get_entry(entry) is not None:
+        return True
+    else:
+        return False
+    
+
+def ResultPage(request):
+    return render(request,"encyclopedia/result.html")
+
+def EntryPage(request,name):
+    content=Convert(name)
+    if content==None:
+        return render(request,"encyclopedia/result.html",{
+                "Result": "Error" ,
+                "descrption":"entry doesn't exists"
+            })
+    else:
+     return render(request,"encyclopedia/entrypage.html",{
+         "title":name,
+         "content":content
+     })
+def Convert(entry):
+    title=util.get_entry(entry)
+    markdowner = markdown.Markdown()
+    if title==None:
+        return None
+    else:
+       return  markdowner.convert(entry)
+        
